f8523ef131690562d1eff52b6203493882476742
unknown <chauhandevansh780@gmail.com>
2026-03-12 23:54:51 +0530

    model formed

---
+++ co2/models/requestmodel.js
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+
+const requestSchema = new mongoose.Schema(
+  {
+    currentLocation: {
+      lat: {
+        type: Number,
+        required: true,
+      },
+      lng: {
+        type: Number,
+        required: true,
+      },
+      address: {
+        type: String,
+      },
+    },
+
+    destination: {
+      lat: {
+        type: Number,
+        required: true,
+      },
+      lng: {
+        type: Number,
+        required: true,
+      },
+      address: {
+        type: String,
+      },
+    },
+
+    engineVehicle: {
+      mileage: {
+        type: Number, 
+        required: true,
+      },
+      unit: {
+        type: String,
+        enum: ['kmpl', 'mpg'],
+        default: 'kmpl',
+      },
+    },
+
+    evVehicle: {
+      distanceRange: {
+        type: Number, 
+        required: true,
+      },
+      unit: {
+        type: String,
+        enum: ['km', 'miles'],
+        default: 'km',
+      },
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+const Request = mongoose.model('Request', requestSchema);
+
+export default Request;
