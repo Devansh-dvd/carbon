9f685eac2da8288f9e53b29197a4556bef06a8a3
unknown <chauhandevansh780@gmail.com>
2026-03-16 18:15:32 +0530

    Final changes

--- routes/maproutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { registerVehicle } from '../controllers/mapcontrollers.js';
-
-const router = express.Router();
-
-router.post('/register', registerVehicle);
-
-export default router;
